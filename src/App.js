c2749832540890f06c67bc4e9ee763c74b39b7ba
karthikjohnn <karthickyamkk2001@gmail.com>
2025-01-25 15:26:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <p className="text-2xl bg-red-200">hello</p>
+      <h1 class="text-3xl font-bold underline bg50">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
